32b3dccae6406487c9cb15ad5b367ad436f6dbb5
Irshad-creates <ansariirshad78601@gmail.com>
2026-06-04 23:26:29 +0530

    DAY 3

--- day 1/app.js
+++ day 1/app.js
@@ -1,41 +1,41 @@
-//  print natural number from 1 to n
+// //  print natural number from 1 to n
 
-class Solution {
-    printNumbers(n) {
-        let arr = []
-       for(let i=1;i<=n;i++){
-        arr.push(i)
-       }
-       return arr
+// class Solution {
+//     printNumbers(n) {
+//         let arr = []
+//        for(let i=1;i<=n;i++){
+//         arr.push(i)
+//        }
+//        return arr
 
-// return array of the factors
-    }
-}
+// // return array of the factors
+//     }
+// }
 
-module.exports = { Solution };
+// module.exports = { Solution };
 
-// in- 5 out-1 2 3 4 5 
+// // in- 5 out-1 2 3 4 5 
 
-/**
- * Recursive function to calculate sum up to N
- * @param {number} n
- * @returns {number}
- */
-function sumUpToN(n) {
-    // Write your logic here
-    let sum= 0;
-    for (let i = 0 ; i <= n ; i++){
-        sum = sum + i
-    }
-    return sum
-}
+// /**
+//  * Recursive function to calculate sum up to N
+//  * @param {number} n
+//  * @returns {number}
+//  */
+// function sumUpToN(n) {
+//     // Write your logic here
+//     let sum= 0;
+//     for (let i = 0 ; i <= n ; i++){
+//         sum = sum + i
+//     }
+//     return sum
+// }
 
-module.exports = { sumUpToN };
-let n=5
-let fact = 1
+// module.exports = { sumUpToN };
+// let n=5
+// let fact = 1
 
-    for(let i = n ; i>=1; i--){
-        fact = fact * i
-    }
-    console.log(fact);
+//     for(let i = n ; i>=1; i--){
+//         fact = fact * i
+//     }
+//     console.log(fact);
 
