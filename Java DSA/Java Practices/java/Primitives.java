76f369ec615ec3047433e2861ad7e3dbc2b62b3b
Irshad-creates <ansariirshad78601@gmail.com>
2026-06-11 23:53:05 +0530

    Remove Main class and main method implementation

---
+++ Java DSA/Java Practices/java/Primitives.java
@@ -0,0 +1,20 @@
+// public class Primitives{
+//     public static void main(String[] args){
+        
+//         // all primitive data type and examples
+        
+//         int rollno = 64;
+//         char letter = 'r';
+//         float marks = "98.45";
+//         double largeDecimalNumber = 232130.2325;
+//         long largerInteger = 32254516;
+//         boolean check = "true";
+
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("hello i am "+ sc);
+        
+    
+
+//     }
+
+// }
