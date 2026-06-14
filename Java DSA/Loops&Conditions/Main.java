abaf19a302b354afacf8d70e6731c92fd6d16627
Irshad-creates <ansariirshad78601@gmail.com>
2026-06-14 23:55:49 +0530

    Refactor salary calculation logic in Main class with improved conditions

--- Java DSA/Loops&Conditions/Main.java
+++ Java DSA/Loops&Conditions/Main.java
@@ -3,11 +3,23 @@
 public class Main{
     public static void main(String[] args){
         int salary = 3000;
+        
+        
+        // if(salary>10000){
+        //     salary += 1000;
+        // }else{
+        //     salary+=2000;
+        // }
+        
+
+        //multi if conditions i.e else if
         if(salary>10000){
-            salary += 1000;
-        }else{
-            salary+=2000;
-        }
+            salary+=1000;
+        }else if(salary>5000){
+            salary += 2000;
+        }else {
+            salary+=5000
+        };
 
         System.out.println(salary);
     } 
