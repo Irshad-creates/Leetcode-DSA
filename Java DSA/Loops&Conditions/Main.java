d737c45ad692e5ffec243301942ee83dc79fdaf6
Irshad-creates <ansariirshad78601@gmail.com>
2026-06-14 23:50:42 +0530

    Add Main class implementation with salary calculation logic with loops and conditions

---
+++ Java DSA/Loops&Conditions/Main.java
@@ -0,0 +1,14 @@
+// loops and conditions 
+
+public class Main{
+    public static void main(String[] args){
+        int salary = 3000;
+        if(salary>10000){
+            salary += 1000;
+        }else{
+            salary+=2000;
+        }
+
+        System.out.println(salary);
+    } 
+}
