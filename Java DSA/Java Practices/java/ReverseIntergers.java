b368b441db07e5d047ed1afb6c9e735f2a9e45ef
Irshad-creates <ansariirshad78601@gmail.com>
2026-06-21 03:49:48 +0530

    Add ReverseIntergers reverse an integer input from the user

---
+++ Java DSA/Java Practices/java/ReverseIntergers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ReverseIntergers {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i =in.nextInt();
+
+        int ans = 0;
+
+        while (i > 0) {
+            int rem = i % 10;
+            
+            i /= 10;
+            
+            ans = ans * 10 + rem;
+        }
+
+        System.out.println(ans);
+
+    }
+}
