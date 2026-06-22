86ed307eff80aaae29f9f708bf7b320697642aae
Irshad-creates <ansariirshad78601@gmail.com>
2026-06-23 01:05:29 +0530

    learnt about functions and methods , Add SumOfTwoNumbers class to calculate and display the sum of two user-input numbers; include compile script for Java files

--- Java DSA/Functions Or Methods/compile.bat
+++ Java DSA/Functions Or Methods/compile.bat
@@ -3,5 +3,11 @@ REM Compile all Java files from java folder to classes folder
 javac -d classes java/*.java
 echo Compilation complete! Class files stored in 'classes' folder.
 
-@REM Step 1: Compile it
+@REM cd ".\Java DSA\Functions Or Methods"
+@REM .\compile.bat
 
+
+
+@REM Step 2: Run the compiled class
+
+@REM java -cp classes [filename]
---
+++ Java DSA/Functions Or Methods/java/SumOfTwoNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SumOfTwoNumbers {
+
+    public static void main(String[] args) {
+        Sum();
+    }
+
+    static void Sum(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int num1 = in.nextInt();
+        
+        System.out.println("Enter the second number");
+        int num2 = in.nextInt();
+        
+        int sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+    }
+}
