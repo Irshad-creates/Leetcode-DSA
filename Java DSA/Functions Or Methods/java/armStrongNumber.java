f791c9a068bbcdcec7a99e145ea6f4b1ea174d5b
Irshad-creates <ansariirshad78601@gmail.com>
2026-07-01 00:56:11 +0530

    Refactor armStrongNumber class to improve readability and restore functionality

--- Java DSA/Functions Or Methods/java/armStrongNumber.java
+++ Java DSA/Functions Or Methods/java/armStrongNumber.java
@@ -2,21 +2,29 @@ import java.util.Scanner;
 
 public class armStrongNumber {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        // Scanner in = new Scanner(System.in);
+        // int n = in.nextInt();
 
         // boolean ans = isArmstrong(n);
-        System.out.println(isArmstrong(n));
+        // System.out.println(isArmstrong(n));
+
+        for(int i = 1 ; i < 10000 ; i++){
+            if(isArmstrong
+                (i)){
+                System.out.println(i);
+            }
+        }
         
     }
 
     static boolean isArmstrong(int n){
         int original = n ;
+        int digits = String.valueOf(n).length();
         int sum = 0;
         while (n > 0){
             int rem = n % 10;
             n = n / 10;
-            sum = sum + rem * rem * rem ;
+            sum += (int) Math.pow(rem, digits) ;
         }
         return original == sum ;
     } 
