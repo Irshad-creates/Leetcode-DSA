4c11267b2d7ded4f305d2a038487c29f8e4855f1
Irshad-creates <ansariirshad78601@gmail.com>
2026-07-10 23:48:38 +0530

    toString method in input.java to disable functionality

--- Java DSA/Arrays/inputs/input.java
+++ Java DSA/Arrays/inputs/input.java
@@ -33,8 +33,8 @@ public class input {
     
     // toString method
 
-        System.out.println(Arrays.toString(arr));
+    //     System.out.println(Arrays.toString(arr));
     
-    }
+    // }
 
 }
