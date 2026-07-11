fe879c2d8ebfc5ceaa23e8548d88df630c623699
Irshad-creates <ansariirshad78601@gmail.com>
2026-07-11 23:16:27 +0530

    array of objects and input.java

--- Java DSA/Arrays/inputs/input.java
+++ Java DSA/Arrays/inputs/input.java
@@ -5,36 +5,50 @@ import java.util.Scanner;
 
 public class input {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        // Scanner in = new Scanner(System.in);
 
+
+        // array of primatives
 
         // manually
 
-        int[]num = new int[5];
-        num[0]=17;
-        num[1]=1245;
-        num[2]=12;
-        num[3]=125;
-        num[4]=112;
+        // int[]num = new int[5];
+        // num[0]=17;
+        // num[1]=1245;
+        // num[2]=12;
+        // num[3]=125;
+        // num[4]=112;
 
-        System.out.println(num[3]);
+        // System.out.println(num[3]);
 
-        // foreach loop
+        // // foreach loop
 
 
-        int[] arr = new int[6];
+        // int[] arr = new int[6];
         
 
-        for(int n : arr){
-            arr[n] = in.nextInt();
-            // System.out.println(n);
-        }
+        // for(int n : arr){
+        //     arr[n] = in.nextInt();
+        //     // System.out.println(n);
+        // }
     
     
     // toString method
 
-    //     System.out.println(Arrays.toString(arr));
+        // System.out.println(Arrays.toString(arr));
     
-    // }
 
+
+    // array of objects
+    Scanner in = new Scanner(System.in);
+
+
+    String[] str = new String[4];
+    for(int i = 0; i < str.length; i++){
+        str[i] = in.next();
+    }
+
+
+    System.out.println(Arrays.toString(str));
+    }
 }
